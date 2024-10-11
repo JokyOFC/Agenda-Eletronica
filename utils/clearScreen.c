fd6255ccade161f7b6a52ab2b993276aac0f10d3
JokyOFC <contato@jokyofc.com>
2024-10-11 11:28:59 -0300

    ✨ feat: identifying which system is and doing the clean thing

--- utils/clearScreen.c
+++ utils/clearScreen.c
@@ -3,3 +3,20 @@
 //
 
 #include "clearScreen.h"
+#include <stdlib.h>
+#include <stdio.h>
+#ifdef _WIN32  // Apenas para Windows
+    #include <windows.h>
+#endif
+
+// Função para limpar a tela de forma portátil
+void clearScreen() {
+    printf("Chamando clearScreen()...\n");
+    #ifdef _WIN32  // Se estiver compilando no Windows
+        system("cls");
+    #elif __linux__ || __APPLE__  // Se estiver compilando no Linux ou MacOS
+        system("clear");
+    #else
+        printf("Não foi possível limpar a tela. Sistema não suportado.\n");
+    #endif
+}
--- utils/clearScreen.h
+++ utils/clearScreen.h
@@ -5,4 +5,6 @@
 #ifndef CLEARSCREEN_H
 #define CLEARSCREEN_H
 
+void clearScreen();
+
 #endif //CLEARSCREEN_H
