f5a3e7e1e566c43c6b7aa2ffdeafe5ff6f588c86
JokyOFC <contato@jokyofc.com>
2024-10-31 12:38:55 -0300

    💥 fix: Fixing problems on windows and adapting to linux too

--- main.c
+++ main.c
@@ -2,19 +2,31 @@
 #include "entities/entitie.h"
 #include "menu/menu.h"
 #include <stdio.h>
-#ifdef _WIN32  // Apenas para Windows
+#include <locale.h>
+
+#ifdef _WIN64
+    #include <windows.h>
+#elif _WIN32  // Apenas para Windows
     #include <windows.h>
 #endif
 
 int main() {
-  #ifdef _WIN32  // Para Windows, definir o console como UTF-8
+  
+  #ifdef _WIN64  // Para Windows, definir o console como UTF-8
     SetConsoleOutputCP(CP_UTF8);
+    setlocale(LC_ALL, "Portuguese");
+  #elif _WIN32
+    SetConsoleOutputCP(CP_UTF8);
+    setlocale(LC_ALL, "Portuguese");
+  #else
+    setlocale(LC_CTYPE, "pt_BR.UTF-8");
   #endif
+  
   Node *contact_list = NULL;
 
   createMockContacts(&contact_list);
   createMockContactWithDependent(&contact_list);
   displayMenu(contact_list);
 
-  return 0;
+  return  0;
 }
